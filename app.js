6a962c547e0b96100a2e6fa17f18b18a02c4d2a0
HIR2009 <70085501+HIR2009@users.noreply.github.com>
2021-09-25 14:11:13 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("slidingNav").style.width = "20%"
+  }
+  
+  function closeNav() {
+    document.getElementById("slidingNav").style.width = "0"
+  }
+
+  
